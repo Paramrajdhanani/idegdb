cbf841b945f64f41f230d9f98bc1469a9d1d1239
Paramrajdhanani <155368765+Paramrajdhanani@users.noreply.github.com>
2026-09-24 15:52:08 +0530

    chore: update wsgi configuration for server deployment

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,5 +1,13 @@
 import os
+import sys
+
+path = "/home/codeforgeIDE/idegdb"
+
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 application = get_wsgi_application()
